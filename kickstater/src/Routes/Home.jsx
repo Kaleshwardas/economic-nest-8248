4d058a7ec6143aa0911404187429dde5832b9780
Kaleshwardas <kabirdas8720@gmail.com>
2022-08-25 00:09:34 +0530

    adding day2 work

---
+++ kickstater/src/Routes/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import { ProductListing } from "./ProductLIsting";
+
+function Home(){
+    return(
+        <div>
+          <Navbar/>
+          <ProductListing/>
+        </div>
+    )
+}
+
+
+export default Home;
